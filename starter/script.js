33388132f774838c26d581bada0af32f962c02df
ketevaniveshaguri <ketevaniveshaguri@gmail.com>
2023-01-28 20:35:49 +0400

    initial update

---
+++ starter/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  window.scrollTo(s1coords.left, s1coords.top);
+});
